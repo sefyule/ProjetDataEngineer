b236ff875c32b84ca5bdb7b7d2bd5b7303df1ef4
CHEKIRINE YOUCEF <ychekiri@localhost.localdomain>
2020-02-04 19:19:26 +0100

    Connection a postgreSQL (Debut de la question 7)

--- src/main/java/SimpleProducer.java
+++ src/main/java/SimpleProducer.java
@@ -7,6 +7,10 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -49,5 +53,15 @@ public class SimpleProducer {
 
 		System.out.println("Fin");
 	}
+    /* Debut de question 7 :
+	try (
+    Connection connection = DriverManager.getConnection("jdbc:postgresql://sqletud.u-pem.fr/login_db", "login", "mdp")) {
+        Statement statement = ((Connection) connection).createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM ciscipdenorm");
+
+
+        while (resultSet.next()) {
+            if (count < 1000) {
+                resultSet.getString("cip7");*/
 
 }
